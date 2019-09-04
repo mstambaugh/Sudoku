67df495321ade0e53b992c4a84f443f1643618bb
molly stambaugh and Rachael Bouissey <rachael@>
2019-09-04 10:43:01 -0700

    create initial directory with dependencies

---
+++ spec/sudokuTest.js
@@ -0,0 +1,10 @@
+describe('Sudoku', function() {
+
+  it('', function() {
+    var triangle = new Triangle(3,4,5);
+    expect(triangle.side1).toEqual(3);
+    expect(triangle.side2).toEqual(4);
+    expect(triangle.side3).not.toEqual(6);
+  });
+
+});
